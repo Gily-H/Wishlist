25066a498c98219bf0f1d836513c1990e83d1a25
Gilman Huang <gilman.huang213@gmail.com>
2021-09-19 14:50:45 -0400

    User model for new registrations. Issue #31

--- server/server.js
+++ server/server.js
@@ -2,20 +2,20 @@ import express from "express";
 import cors from "cors";
 
 // import routes
-import homeRoutes from "./routes/home.js";
-import loginRoutes from "./routes/login.js";
-import signupRoutes from "./routes/signup.js";
-import wishlistRoutes from "./routes/wishlist.js";
- 
+import homeRouter from "./routes/home.js";
+import loginRouter from "./routes/login.js";
+import registerRouter from "./routes/register.js";
+import wishlistRouter from "./routes/wishlist.js";
+
 const app = express();
 
 // middlwares will always run before everything
 app.use(cors());
 app.use(express.json()); // REMEMBER .json() requires parenthesis
 
-app.use("/", homeRoutes);
-app.use("/login", loginRoutes);
-app.use("/signup", signupRoutes);
-app.use("/wishlist", wishlistRoutes);
+app.use("/", homeRouter);
+app.use("/login", loginRouter);
+app.use("/register", registerRouter);
+app.use("/wishlist", wishlistRouter);
 
 export default app;
