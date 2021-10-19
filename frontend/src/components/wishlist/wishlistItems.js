ecd9a52ba19d57f6e360382f9ebd3e16265b496e
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2021-10-19 11:33:37 -0400

    added button link to direct to item form page

--- frontend/src/components/wishlist/wishlistItems.js
+++ frontend/src/components/wishlist/wishlistItems.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import WishlistDataService from "../../services/wishlistService.js";
 import "../../styles/itemsTable.css";
 
@@ -70,6 +70,9 @@ const WishlistItems = () => {
       ) : (
         <ItemsTable items={wishlistItems} />
       )}
+      <Link to={`/wishlist/${wishlistId}/add-item`}>
+        <button>Add an item</button>
+      </Link>
     </>
   );
 };
